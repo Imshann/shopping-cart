dfd0357522a65ab8a90172c3a1e9ad117bc5417f
zhiyi <395939667@qq.com>
2015-09-12 17:04:32 +0800

    change sideorderlist.html

--- sea-modules/app/goods.js
+++ sea-modules/app/goods.js
@@ -6,12 +6,36 @@ define(['jquery'], function (require, exports, module) {
     module.exports = {
         init: function () {
             this.columnChange();
+			this.doubleChange();
+			this.upanddown();
         },
 
         columnChange: function () {
             $('.single, .double').click(function () {
-                alert(1);
+                //alert(1);
             })
-        }
+        },
+		//双列功能
+		doubleChange: function () {
+			$('.double').click(function(){
+				$('.main-left').css({width:'460px'});
+				$('.double-c').css({display:"block"});
+				$(".right-menu").css({display:"none"});
+			})
+		},
+		//上拉和下拉功能
+		upanddown: function () {
+			$(".up-down").click(function(){
+				var a = $(this).parent().next(".item-list").css("display");
+				if(a=="block"){
+					$(this).parent().next(".item-list").css("display","none");
+					$(this).find("img").attr("src","static/images/direction_2.png");
+				}else{
+					$(this).parent().next(".item-list").css("display","block");
+					$(this).find("img").attr("src","static/images/direction_1.png");
+				};
+				console.log(a);
+			});
+		}
     };
 });
